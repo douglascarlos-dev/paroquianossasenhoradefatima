37c277742fda9c444a1bda3538cbb61f6a997899
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2026-08-16 16:36:15 -0300

    :rocket: Atualização de atualização de envio de e-mail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -88,7 +88,7 @@ export class AppComponent {
     payload.append('anexos[]', file, file.name);
   }
 
-  this.http.post('http://localhost:8086/send-email.php', payload, { responseType: 'json' }).subscribe({
+  this.http.post('http://douglascarlos.infinityfree.me/send-email.php', payload, { responseType: 'json' }).subscribe({
     next: (res: any) => {
       this.isSubmitting = false;
       this.isError = false;
